61fb288cefee36daebae1b7d53f22f5fd502d34d
Davide <setti.davide89@gmail.com>
2023-12-12 10:18:43 +0100

    Improve

--- netconstrainterror/tests.py
+++ netconstrainterror/tests.py
@@ -18,6 +18,16 @@ class ModelTestCase(TestCase):
             instance.save()
         print(ctx.captured_queries, logger.output)
 
+    def test_creating_and_save_with_none(self):
+        instance = AssignableIP.objects.create(current_ip=None, network="192.168.0.0/24")
+        instance = AssignableIP.objects.get(pk=instance.pk)
+        with (
+            CaptureQueriesContext(connection) as ctx
+        ):
+            instance.full_clean()
+        instance.save()
+        print(ctx.captured_queries)
+
     def test_instantiating(self):
         instance = AssignableIP(current_ip="192.168.0.1", network="192.168.0.0/24")
         with (
@@ -28,3 +38,12 @@ class ModelTestCase(TestCase):
         with self.assertRaisesMessage(InternalError, 'current transaction is aborted, commands ignored until end of transaction block'):
             instance.save()
         print(ctx.captured_queries, logger.output)
+
+    def test_instantiating_with_none(self):
+        instance = AssignableIP(current_ip=None, network="192.168.0.0/24")
+        with (
+            CaptureQueriesContext(connection) as ctx
+        ):
+            instance.full_clean()
+        instance.save()
+        print(ctx.captured_queries)
