58eab79e6009364714c4d9eb84d3984baf351edb
Davide <setti.davide89@gmail.com>
2023-12-12 10:07:41 +0100

    also log queries

--- netconstrainterror/tests.py
+++ netconstrainterror/tests.py
@@ -1,5 +1,6 @@
-from django.db import InternalError
+from django.db import InternalError, connection
 from django.test import TestCase
+from django.test.utils import CaptureQueriesContext
 from .models import AssignableIP
 
 
@@ -8,16 +9,22 @@ class ModelTestCase(TestCase):
     def test_creating_and_save(self):
         instance = AssignableIP.objects.create(current_ip="192.168.0.1", network="192.168.0.0/24")
         instance = AssignableIP.objects.get(pk=instance.pk)
-        with self.assertLogs('django.db.models') as logger:
+        with (
+            self.assertLogs('django.db.models') as logger,
+            CaptureQueriesContext(connection) as ctx
+        ):
             instance.full_clean()
         with self.assertRaisesMessage(InternalError, 'current transaction is aborted, commands ignored until end of transaction block'):
             instance.save()
-        print(logger.output)
+        print(ctx.captured_queries, logger.output)
 
     def test_instantiating(self):
         instance = AssignableIP(current_ip="192.168.0.1", network="192.168.0.0/24")
-        with self.assertLogs('django.db.models') as logger:
+        with (
+            self.assertLogs('django.db.models') as logger,
+            CaptureQueriesContext(connection) as ctx
+        ):
             instance.full_clean()
         with self.assertRaisesMessage(InternalError, 'current transaction is aborted, commands ignored until end of transaction block'):
             instance.save()
-        print(logger.output)
+        print(ctx.captured_queries, logger.output)
