61f1ed857c739efd453b657566dc66363b4942f7
Davide <setti.davide89@gmail.com>
2023-12-12 09:56:05 +0100

    Reproduce case

--- netconstrainterror/tests.py
+++ netconstrainterror/tests.py
@@ -1,32 +1,17 @@
-from ipaddress import ip_address, ip_network
-from django.db import connection
 from django.test import TestCase
-from django.test.utils import CaptureQueriesContext
 from .models import AssignableIP
 
 
 class ModelTestCase(TestCase):
 
-    def test_with_null_ip(self):
-        instance = AssignableIP(current_ip=None, network="192.168.0.0/24")
-        with CaptureQueriesContext(connection) as ctx:
-            instance.full_clean()
-        print('Null and string', ctx.captured_queries)
+    def test_with_loading_from_db(self):
+        instance = AssignableIP.objects.create(current_ip="192.168.0.1", network="192.168.0.0/24")
 
-    def test_with_not_null_ip(self):
-        instance = AssignableIP(current_ip="192.168.0.1", network="192.168.0.0/24")
-        with CaptureQueriesContext(connection) as ctx:
+        instance = AssignableIP.objects.select_for_update().get(pk=instance.pk)
+        with self.assertLogs('django.db.models') as logger:
             instance.full_clean()
-        print('string and string', ctx.captured_queries)
-
-    def test_with_not_null_ip_bis(self):
-        instance = AssignableIP(current_ip=ip_address("192.168.0.1"), network=ip_network("192.168.0.0/24"))
-        with CaptureQueriesContext(connection) as ctx:
-            instance.full_clean()
-        print('ipaddres and ipnetwork', ctx.captured_queries)
-
-    def test_with_null_ip_bis(self):
-        instance = AssignableIP(current_ip=None, network=ip_network("192.168.0.0/24"))
-        with CaptureQueriesContext(connection) as ctx:
-            instance.full_clean()
-        print('null and ipnetwork', ctx.captured_queries)
+        try:
+            instance.save()
+        except:
+            pass
+        print(logger.output)
