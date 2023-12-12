1896c0ed5a3b9d941c9e4fdab0c72081c9e690d7
Davide <setti.davide89@gmail.com>
2023-12-12 10:03:10 +0100

    Fix tests

--- netconstrainterror/tests.py
+++ netconstrainterror/tests.py
@@ -1,17 +1,23 @@
+from django.db import InternalError
 from django.test import TestCase
 from .models import AssignableIP
 
 
 class ModelTestCase(TestCase):
 
-    def test_with_loading_from_db(self):
+    def test_creating_and_save(self):
         instance = AssignableIP.objects.create(current_ip="192.168.0.1", network="192.168.0.0/24")
-
-        instance = AssignableIP.objects.select_for_update().get(pk=instance.pk)
+        instance = AssignableIP.objects.get(pk=instance.pk)
         with self.assertLogs('django.db.models') as logger:
             instance.full_clean()
-        try:
+        with self.assertRaisesMessage(InternalError, 'current transaction is aborted, commands ignored until end of transaction block'):
             instance.save()
-        except:
-            pass
+        print(logger.output)
+
+    def test_instantiating(self):
+        instance = AssignableIP(current_ip="192.168.0.1", network="192.168.0.0/24")
+        with self.assertLogs('django.db.models') as logger:
+            instance.full_clean()
+        with self.assertRaisesMessage(InternalError, 'current transaction is aborted, commands ignored until end of transaction block'):
+            instance.save()
         print(logger.output)
